73d606d54aeb28a008c4a09d84ff7a2ead6462b2
Andreas Ventzel <andreas.ventzel@gmail.com>
2024-10-30 13:46:22 +0100

    Initial commit for project

---
+++ Week4_CsvReader/Week4_CsvReader/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Week4_CsvReader.Services;
+using Week4_CsvReader.Services.Interfaces;
+Console.WriteLine("Hello, World!");
+var filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SampleData\\country_full.csv");
+var fileOut = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SampleData\\country_out.csv");
+var fileAfrica = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SampleData\\country_africa.csv");
+
+ICsvFileReader fileReader = new CsvFileReader();
+ICsvPrinter printer = new CsvPrinter();
+ICsvSorter sorter = new CsvSorter();
+ICsvExporter exporter = new CsvExporter();
+
+//4 
+var records = fileReader.ReadCsv(filepath);
+var filteredRecords = fileReader.ReadFilteredCsv(filepath, "region", "Africa");
+printer.PrintCsv(filteredRecords);
+exporter.ExportToCsv(filteredRecords, fileAfrica);
